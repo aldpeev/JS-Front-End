18f998c1d5bbb8710e95ff95cb8aa935d9ace14e
Aleksandar Peev <149875746+aldpeev@users.noreply.github.com>
2023-11-14 12:39:45 +0200

    remove files

--- L01_JS_Syntax_Fundamentals/Lab/P02StudentInformation.js
+++
@@ -1,7 +0,0 @@
-// Name: {student name}, Age: {student age}, Grade: {student grade}
-
-function printStudentInfo(name, age, grade){
-    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`);
-}
-
-printStudentInfo("Pesho", 10, 5.666);
